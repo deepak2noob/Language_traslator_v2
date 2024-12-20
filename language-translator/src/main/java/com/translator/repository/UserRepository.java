9ae5f5ae82901367bfc525aefc4ac3eab5c8391d
Deepak_sleeping.py <personal1use1deepak@gmail.com>
2024-12-20 14:18:17 +0530

    Delete language-translator directory

--- language-translator/src/main/java/com/translator/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.translator.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.translator.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
